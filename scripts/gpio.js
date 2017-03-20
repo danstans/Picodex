d0f9e59dfcaf3b68ce70ea5c1b9cf8e14c189d7d
Daniel Stansberry <stansberry2295@gmail.com>
2017-03-19 22:06:03 -0700

    added a function to convert strings to byteArrays

--- scripts/gpio.js
+++ scripts/gpio.js
@@ -1,13 +1,36 @@
 var pico8_gpio = new Array(128);
+var lastPokemon = 0
+var currentPokemon = pico8_gpio[1];
 
+function convertStringToByteArray(pokeString) {
+  var alpha = 'abcdefghijklmnopqrstuvwxyz'
+  var byteArray = []
+  for (pokeChar in pokeString) {
+    byteArray.push(alpha.indexOf(pokeString[pokeChar]));
+  }
+  return byteArray;
+}
+
+function sendNameToLua(ByteArray) {
+
+}
 
 function onRender() {
-    if (pico8_gpio[0] == 0) {
+    currentPokemon = pico8_gpio[1];
+    if (lastPokemon != currentPokemon) {
+      lastPokemon = currentPokemon
+      // console.log(`The current pokemon value is ${currentPokemon}`);
+      var pokemonPromise = getPokemonStats(currentPokemon);
+      pokemonPromise.then(pokemon => {
+        var nameByteArray = convertStringToByteArray(pokemon.Name);
+        sendNameToLua(nameByteArray);
+        // console.log(pokemon.Name);
+        // console.log(pokemon.Weight);
+        // console.log(pokemon.Height);
+      });
 
     }
     window.requestAnimationFrame(onRender);
 }
 
-var pokemonPromise = getPokemonStats(1);
-pokemonPromise.then(console.log);
 window.requestAnimationFrame(onRender);
